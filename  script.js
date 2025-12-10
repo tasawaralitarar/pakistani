59935fb237ec4540b15fc9f755a223047d34b887
25S01( Ali Tasawar) <tasawar@chuo-ai.jp>
2025-12-10 01:54:13 +0000

    add html css js

---  script.js
+++  script.js
@@ -2,18 +2,18 @@ let redCount = 0;
 let blueCount = 0;
 let yellowcount=0;
 
-document.getElementById("redBtn").addEventListener("click", () => {
+document.getElementById("redBtn").addEventListener("click", (1) => {
   redCount++;
   document.getElementById("message").innerText =
     "赤ボタンを " + redCount + " 回押しました";
 });
 
-document.getElementById("blueBtn").addEventListener("click", () => {
+document.getElementById("blueBtn").addEventListener("click", (1) => {
   blueCount++;
   document.getElementById("message").innerText =
     "青ボタンを " + blueCount + " 回押しました";
 });
-document.getElementById("yellowbtn").addEventListener("click", () => {
+document.getElementById("yellowbtn").addEventListener("click", (1) => {
   blueCount++;
   document.getElementById("message").innerText =
     "青ボタンを " + blueCount + " 回押しました";
